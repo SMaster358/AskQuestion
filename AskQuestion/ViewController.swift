7e07973af36dc5bb3a9f6fa30b686487954dd809
SMaster358 <samuelmaster10@gmail.com>
2020-10-08 09:51:39 -0400

    Initial Commit

---
+++ AskQuestion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AskQuestion
+//
+//  Created by Samuel Master on 10/8/20.
+//  Copyright © 2020 PogApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
